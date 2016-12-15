27b49bff476f3de664ed58c954766e49e0d65849
Gudmund Grov <gudmundgrov@gmail.com>
2016-12-15 14:47:02 +0000

    Started forall statement

---
+++ Source/Dafny/Tacny/Language/ForallStmt.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using Microsoft.Boogie;
+using Tacny;
+using Formal = Microsoft.Dafny.Formal;
+using Type = Microsoft.Dafny.Type;
+using Microsoft.Dafny;
+
+
+namespace Tacny.Language
+{
+    class ForallStmt
+    {
+        /*
+         *  TODO: generalise: this could also be a meta-level variable or tactic application
+         */
+        [Pure]
+        public static bool IsForallShape(Expression e){
+            Contract.Requires(e != null);
+
+            if (e is Microsoft.Dafny.ForallExpr){
+                var fall = e as Microsoft.Dafny.ForallExpr;
+                if (fall.LogicalBody() is BinaryExpr){
+                    var body = fall.LogicalBody() as BinaryExpr;
+                    if (body?.Op == BinaryExpr.Opcode.Imp){
+                        return true;
+                    }else{
+                        return false;
+                    }    
+                }
+            }
+            return false;
+        }
+
+        public static IEnumerable<ProofState> EvalInit(Statement statement, ProofState state0){
+            Contract.Requires(statement != null);
+            Contract.Requires(statement is TacnyForallStmt);
+
+            var stmt = statement as TacnyForallStmt;
+            var e = stmt.Spec;
+            var attr = stmt.Attributes;
+            
+            Contract.Assert(e != null && IsForallShape(e));
+            
+            return null;
+        }
+    }
+}
--- Source/Dafny/Tacny/Language/OrChoiceStmt.cs
+++ Source/Dafny/Tacny/Language/OrChoiceStmt.cs
@@ -22,19 +22,16 @@ namespace Tacny.Language
         public static bool IsNonDeterministic(Statement statement)
         {
             Contract.Requires(statement != null);
-            if (statement is IfStmt)
-            {
+            if (statement is IfStmt){
                 var ifstmt = statement as IfStmt;
                 if (ifstmt.Guard == null)
                     return true;
                 else
                     return false;
             }
-            if (statement is AlternativeStmt)
-            {
+            if (statement is AlternativeStmt){
                 var ifstmt = statement as AlternativeStmt;
-                foreach (GuardedAlternative a in ifstmt.Alternatives)
-                {
+                foreach (GuardedAlternative a in ifstmt.Alternatives){
                     if (a.Guard != null)
                         return false;
                 }
@@ -44,35 +41,29 @@ namespace Tacny.Language
         }
 
 
-        public IEnumerable<ProofState> Eval(Statement statement, ProofState state0)
-        {
+        public IEnumerable<ProofState> Eval(Statement statement, ProofState state0){
             Contract.Requires(statement != null);
             Contract.Requires(IsNonDeterministic(statement));
             //Contract.Requires(statement is TacnyCasesBlockStmt);
 
            List<BlockStmt> choices = new List<BlockStmt>();
 
-            if (statement is IfStmt)
-            {
+            if (statement is IfStmt){
                 var ifstmt = statement as IfStmt;
                 if (ifstmt.Thn != null)
                     choices.Add(ifstmt.Thn as BlockStmt);
                 if (ifstmt.Els != null)
                     choices.Add(ifstmt.Els as BlockStmt);
             }
-            if (statement is AlternativeStmt)
-            {
+            if (statement is AlternativeStmt){
                 var ifstmt = statement as AlternativeStmt;
-                foreach (GuardedAlternative a in ifstmt.Alternatives)
-                {
+                foreach (GuardedAlternative a in ifstmt.Alternatives){
                    if (a.Body != null && a.Body.Count != 0)
                         choices.Add(new BlockStmt(a.Body.First().Tok, a.Body.Last().EndTok, a.Body));
                 }
             }
-
             ProofState state = null;
-            foreach (BlockStmt choice in choices)
-            {
+            foreach (BlockStmt choice in choices){
                 state = state0.Copy();
                 state.AddNewFrame(choice.SubStatements.ToList(), IsPartial);
                 yield return state;
